0ba5fdc295510041689867ec6d223df2196eb9cb
Raphael Fleury <raphael.fleury9@gmail.com>
2021-04-22 11:52:55 -0300

    backend: Populando banco de dados

---
+++ restapi/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO CLINICA(nome) VALUES('Petz');
+
+INSERT INTO CLIENTE(nome, email, endereco, telefone, clinica_id) VALUES('Manoel Siqueira', 'manoelsiqueira@gmail.com', 'Rua Batista Pereira, 32 - Santos/SP', '13981446177', 1);
+INSERT INTO CLIENTE(nome, email, endereco, telefone, clinica_id) VALUES('Laura Alves', 'lalves@gmail.com', 'Rua Ver. Henrique Soler, 64 - Santos/SP', '13981564787', 1);
+INSERT INTO CLIENTE(nome, email, endereco, telefone, clinica_id) VALUES('José Damasceno', 'jdamasceno3@gmail.com', 'Rua Rangel Pestana, 4 - São Vicente/SP', '13981334617', 1);
+INSERT INTO CLIENTE(nome, email, endereco, telefone, clinica_id) VALUES('Mariella Campos', 'maricampos@outlook.com', 'Rua Dona Ana Nery, 50 - Santos/SP', '13981956194', 1);
+
+INSERT INTO PET(nome, especie, raca, sexo, castrado, clinica_id, dono_id) VALUES('Rex', 'Cachorro', 'Labrador', 'MASCULINO', true, 1, 1);
+INSERT INTO PET(nome, especie, raca, sexo, castrado, clinica_id, dono_id) VALUES('Mel', 'Gato', '', 'FEMININO', true, 1, 2);
+INSERT INTO PET(nome, especie, raca, sexo, castrado, clinica_id, dono_id) VALUES('Tonkay', 'Gato', '', 'MASCULINO', false, 1, 3);
+INSERT INTO PET(nome, especie, raca, sexo, castrado, clinica_id, dono_id) VALUES('José', 'Papagaio', '', 'MASCULINO', false, 1, 4);
